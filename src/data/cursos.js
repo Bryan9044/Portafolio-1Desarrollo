9b63cdd527e712e364f10a9715718de39452ada7
Bryan Londoño Marchena <bryanlondo9044@gmail.com>
2025-10-16 14:24:12 -0600

    Se agregi la información de los cursos correspondientes

--- src/data/cursos.js
+++ src/data/cursos.js
@@ -7,31 +7,61 @@ const cursos = [
     image: PaginasWebImg,
     items: [
       {
-        title: 'Laboratorio 1',
-        description: 'Implementar un formulario web',
-        typeEvaluation: 'Laboratorio',
-        deliveryDate: '28/8/2025',
-        technologiesInvolved: 'HTML',
+        title: 'Investigación 1',
+        description: 'Replicar datos en dos bases de datos en un mismo servidor',
+        typeEvaluation: 'Investigación',
+        deliveryDate: '23/9/2025',
+        technologiesInvolved: 'SQL Server',
         linkRepo: '#',
+        linkDeploy: '#'
+      },
+      {
+        title: 'Tarea 1',
+        description: 'API REST para la base de Adventure Works',
+        typeEvaluation: 'Tarea',
+        deliveryDate: '14/9/2025',
+        technologiesInvolved: 'Node.js, Express, SQL Server',
+        linkRepo: 'https://github.com/2025-Semestre-2/tarea-1-Bryan9044',
+        linkDeploy: '#'
+      },
+      {
+        title: 'Primer proyecto',
+        description: 'Hacer modulos de la base WideWorldImporters y con una API mostrarlos en un frontend',
+        typeEvaluation: 'Proyecto',
+        deliveryDate: '12/10/2025',
+        technologiesInvolved: 'SQL server, CSS, Vue, .net core',
+        linkRepo: 'https://github.com/2025-Semestre-2/proyecto-1-Bryan9044',
         linkDeploy: '#'
       },
       {
         title: 'Quizz 1',
-        description: 'Preguntas de HTML y CSS',
+        description: 'Con ciertas tablas mostrar pagos, viajes, clientes, total y subtotal',
         typeEvaluation: 'Quizz',
-        deliveryDate: '29/8/2025',
-        technologiesInvolved: 'HTML y CSS',
-        linkRepo: '#',
+        deliveryDate: '27/08/2025',
+        technologiesInvolved: 'SQL server',
+        linkRepo: '',
         linkDeploy: '#'
+
       },
       {
-        title: 'Proyecto Final',
-        description: 'Crear un sitio completo',
-        typeEvaluation: 'Proyecto',
-        deliveryDate: '30/8/2025',
-        technologiesInvolved: 'HTML, CSS, JS',
+        title: 'Quizz 2',
+        description: 'Calcular el costo de ciertas transferencias y Join con la información dada',
+        typeEvaluation: 'Quizz',
+        deliveryDate: '24/09/2025',
+        technologiesInvolved: 'Word',
         linkRepo: '#',
         linkDeploy: '#'
+
+      },
+      {
+        title: 'Quizz 3',
+        description: 'A partir de una tabla separar la tabla y mantener datos originales según sucursal',
+        typeEvaluation: 'Quizz',
+        deliveryDate: '15/10/2025',
+        technologiesInvolved: 'SQL Server',
+        linkRepo: '#',
+        linkDeploy: '#'
+
       }
     ]
   },
@@ -42,32 +72,68 @@ const cursos = [
     image: PaginasWebImg,
     items: [
       {
-        title: 'Laboratorio 1',
-        description: 'Implementar un formulario web',
-        typeEvaluation: 'Laboratorio',
-        deliveryDate: '28/8/2025',
-        technologiesInvolved: 'HTML',
+        title: 'Examen 1',
+        description: 'Responder preguntas teoricas y armar arbol sintactico',
+        typeEvaluation: 'Examen',
+        deliveryDate: '02/10/2025',
+        technologiesInvolved: 'Jflex y word',
         linkRepo: '#',
+        linkDeploy: '#'
+      },
+      {
+        title: 'Tarea 1',
+        description: 'Crear la gramática del cursos siguiendo los lineamientos indicados',
+        typeEvaluation: 'Tarea',
+        deliveryDate: '12/09/2025',
+        technologiesInvolved: 'Bloc de notas',
+        linkRepo: 'https://github.com/DylanRodriguez22/Tarea1_Compiladores',
+        linkDeploy: '#'
+      },
+      {
+        title: 'Tarea 2',
+        description: 'Investigar temas de MIPS y hacer código con respecto al #1 proyecto',
+        typeEvaluation: 'Tarea',
+        deliveryDate: '18/09/2025',
+        technologiesInvolved: 'Word y MARS',
+        linkRepo: '#',
+        linkDeploy: '#'
+      },
+      {
+        title: 'Proyecto 1',
+        description: 'Crear lexer y parser a partir de la tarea#1',
+        typeEvaluation: 'Proyecto',
+        deliveryDate: '06/10/2025',
+        technologiesInvolved: 'word, Jflex, Cup',
+        linkRepo: 'https://github.com/Bryan9044/ProyectoCompi-1',
         linkDeploy: '#'
       },
       {
         title: 'Quizz 1',
-        description: 'Preguntas de HTML y CSS',
+        description: 'Mentimeter sobre temas vistos en clase',
         typeEvaluation: 'Quizz',
-        deliveryDate: '29/8/2025',
-        technologiesInvolved: 'HTML y CSS',
+        deliveryDate: '19/08/2025',
+        technologiesInvolved: 'Mentimeter',
         linkRepo: '#',
         linkDeploy: '#'
       },
       {
-        title: 'Proyecto Final',
-        description: 'Crear un sitio completo',
-        typeEvaluation: 'Proyecto',
-        deliveryDate: '30/8/2025',
-        technologiesInvolved: 'HTML, CSS, JS',
+        title: 'Quizz 2',
+        description: 'Responder preguntas teoricas vistas en clase',
+        typeEvaluation: 'Quizz',
+        deliveryDate: '18/09/2025',
+        technologiesInvolved: 'Word',
         linkRepo: '#',
         linkDeploy: '#'
-      }
+      },
+      {
+        title: 'Quizz 3',
+        description: 'Hacer tabla Dtran y arbol sintactico',
+        typeEvaluation: 'Quizz',
+        deliveryDate: '23/09/2025',
+        technologiesInvolved: 'Word',
+        linkRepo: '#',
+        linkDeploy: '#'
+      },
     ]
   },
   {
@@ -77,31 +143,13 @@ const cursos = [
   image: PaginasWebImg,
   items: [
     {
-      title: 'Laboratorio 1',
-      description: 'Implementar un formulario web',
-      typeEvaluation: 'Laboratorio',
-      deliveryDate: '28/8/2025',
-      technologiesInvolved: 'HTML',
+      title: 'Tarea 1',
+      description: 'Crear contenido digital sobre lo que hemos visto en clase',
+      typeEvaluation: 'Tarea',
+      deliveryDate: '16/10/2025',
+      technologiesInvolved: 'Youtube, Canva',
       linkRepo: '#',
-      linkDeploy: '#'
-    },
-    {
-      title: 'Quizz 1',
-      description: 'Preguntas de HTML y CSS',
-      typeEvaluation: 'Quizz',
-      deliveryDate: '29/8/2025',
-      technologiesInvolved: 'HTML y CSS',
-      linkRepo: '#',
-      linkDeploy: '#'
-    },
-    {
-      title: 'Proyecto Final',
-      description: 'Crear un sitio completo',
-      typeEvaluation: 'Proyecto',
-      deliveryDate: '30/8/2025',
-      technologiesInvolved: 'HTML, CSS, JS',
-      linkRepo: '#',
-      linkDeploy: '#'
+      linkDeploy: 'https://www.youtube.com/watch?v=0rGIRR71tMU'
     }
   ]
   },
@@ -164,27 +212,126 @@ const cursos = [
       technologiesInvolved: 'js,html',
       linkRepo: '#',
       linkDeploy: '#'
-    },  
+    },
     {
-      title: 'Quizz 1',
-      description: 'Preguntas de HTML y CSS',
-      typeEvaluation: 'Quizz',
-      deliveryDate: '29/8/2025',
-      technologiesInvolved: 'HTML y CSS',
+      title: 'Laboratorio 7',
+      description: 'Presentación sobre tema asignado en clase',
+      typeEvaluation: 'Laboratorio',
+      deliveryDate: '24/9/2025',
+      technologiesInvolved: 'Canva',
       linkRepo: '#',
       linkDeploy: '#'
     },
     {
-      title: 'Proyecto Final',
-      description: 'Crear un sitio completo',
-      typeEvaluation: 'Proyecto',
-      deliveryDate: '30/8/2025',
-      technologiesInvolved: 'HTML, CSS, JS',
+      title: 'Laboratorio 8',
+      description: 'API REST con JWT y AuthKey',
+      typeEvaluation: 'Laboratorio',
+      deliveryDate: '03/10/2025',
+      technologiesInvolved: 'Node.js, Express, Postman',
+      linkRepo: 'https://github.com/Bryan9044/Lab8',
+      linkDeploy: '#'
+    },  
+    {
+      title: 'Laboratorio 9',
+      description: 'API REST con GraphQL',
+      typeEvaluation: 'Laboratorio',
+      deliveryDate: '29/8/2025',
+      technologiesInvolved: 'TypeScript, Node.js, Express, GraphQL',
       linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Proyecto 1',
+      description: 'Crear maqueta en el frontend sobre la banca web que nos pide el cliente',
+      typeEvaluation: 'Proyecto',
+      deliveryDate: '28/9/2025',
+      technologiesInvolved: 'Vue, CSS, JS',
+      linkRepo: 'https://github.com/Seraf1n0/BancaWeb',
+      linkDeploy: '#'
+    }
+,
+    {
+      title: 'Portafolio 1',
+      description: 'Crear portafolio personal con respecto al curso en un framework moderno',
+      typeEvaluation: 'Portafolio',
+      deliveryDate: '19/9/2025',
+      technologiesInvolved: 'Vue, CSS, JS',
+      linkRepo: 'https://github.com/Bryan9044/Portafolio-1Desarrollo',
       linkDeploy: '#'
     }
   ]
   },
+ {
+  id: 'QA',
+  nombre: 'Aseguramiento de la calidad de software',
+  description: 'Aprender principios y estandares para asegurar la calidad del software desarrollado',
+  image: PaginasWebImg,
+  items: [
+    {
+      title: 'Investigación 1',
+      description: 'Recopilar información sobre el estándar IEEE 15288 y 12207',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '21/8/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Investigación 2',
+      description: 'Recopilar información sobre el estándar IEEE 730',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '5/9/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Investigación 3',
+      description: 'Recopilar información sobre el estándar IEEE 829',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '5/9/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Investigación 4',
+      description: 'Recopilar información sobre el estándar IEEE 830',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '19/9/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Investigación 5',
+      description: 'Recopilar información sobre el estándar IEEE 1028',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '3/10/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Investigación 6',
+      description: 'Recopilar información sobre el estándar IEEE 1044',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '10/10/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+    {
+      title: 'Investigación 7',
+      description: 'Recopilar información sobre el estándar IEEE 1008',
+      typeEvaluation: 'Investigación',
+      deliveryDate: '20/10/2025',
+      technologiesInvolved: 'Word',
+      linkRepo: '#',
+      linkDeploy: '#'
+    },
+ ]
+ }
 
 ]
 
