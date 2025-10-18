2cfc8b82a87fb045087602c5b3782970c58fb98b
Bryan Londoño Marchena <bryanlondo9044@gmail.com>
2025-10-17 23:14:22 -0600

    Sección personal se hace la integración de comentario (EN PROCESO)

--- src/App.vue
+++ src/App.vue
@@ -48,39 +48,7 @@ watch(() => route.fullPath, () => {
 
   <RouterView :key="$route.fullPath" />
 
-  <footer class="footer">
-     <div class="container">
-      <div class="row">
-        <div class="footer-col">
-          <h4>Acerca de mí</h4>
-          <ul>
-            <li><a href="#">Sobre mí</a></li>
-            <li><a href="#">Cursos</a></li>
-            <li><a href="#">privacy policy</a></li>
-            <li><a href="#">affiliate program</a></li>
-          </ul>
-        </div>
-        <div class="footer-col">
-          <h4>online shop</h4>
-          <ul>
-            <li><a href="#">watch</a></li>
-            <li><a href="#">bag</a></li>
-            <li><a href="#">shoes</a></li>
-            <li><a href="#">dress</a></li>
-          </ul>
-        </div>
-        <div class="footer-col">
-          <h4>Mis redes</h4>
-          <div class="social-links">
-            <a href="#"><i class="fab fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-          </div>
-        </div>
-      </div>
-     </div>
-  </footer>
+
 
 </template>
 
--- src/views/SeccionPerso.vue
+++ src/views/SeccionPerso.vue
@@ -1,5 +1,62 @@
 <script setup>
 import cards3d from '@/components/Cards3dv.vue'
+import gifAnimacion from '@/assets/gif.gif'
+import { ref, onMounted } from 'vue'
+
+
+const comentarios = ref([])
+
+
+const nuevoComentario = ref({
+    Nombre: '',
+    Comentario: ''
+})
+
+async function CargaComentarios() {
+    try {
+        const res = await fetch('/.netlify/functions/guardarComentarios')
+        const data = await res.json()
+        comentarios.value = data.Comentarios
+    } catch (error) {
+        console.error('Error al cargar los comentarios:', error)
+    }
+
+    
+}
+
+
+async function agregarComentario() {
+    if (!nuevoComentario.value.Comentario) {
+        alert('Por favor escribe un comentario :c')
+        return
+    }
+
+    try {
+        const res = await fetch('/.netlify/functions/guardarcomentario', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(nuevoComentario.value)
+        })
+
+        const data = await res.json()
+        console.log('Comentario agregado:', data.mensaje)
+
+        await CargaComentarios()
+
+        nuevoComentario.value.Nombre = ''
+        nuevoComentario.value.Comentario = ''   
+    } catch (err) {
+        console.error('Error al agregar comentario:', err)
+    }
+}
+
+onMounted(() => {
+    CargaComentarios()
+})
+
+
+
+
 </script>
 
 <template>
@@ -19,6 +76,24 @@ import cards3d from '@/components/Cards3dv.vue'
             que uno imagina y sueña.
         </p>
     </div>
+        <div class="comments"> 
+        <p>Si gustas puedes dejar un mensaje para mí ya sea de cualquier cosa que quieras mencionar </p>
+        <img :src="gifAnimacion" alt="Git de persona con cara divertida con laptop escribiendo" class="img-gif">
+        <img :src="gifAnimacion" alt="Git de persona con cara divertida con laptop escribiendo" class="img-gif2">
+        <input type="text" placeholder="Escribe aquí..."  v-model="nuevoComentario.Nombre" />
+        <input type="text" placeholder="Escribe aquí..."  v-model="nuevoComentario.Comentario" />
+
+        </div>
+
+        <button @click="agregarComentario">Enviar</button>
+
+        <div v-if="comentarios.length > 0" class="comentarios-lista">
+            <h3>Comentarios recientes</h3>
+            <div v-for="(c, i) in comentarios" :key="i" class="comentario-item">
+            <strong>{{ c.Nombre }}</strong>: {{ c.Comentario }}
+            </div>
+        </div>
+
 </div>  
 </template>
 
@@ -54,5 +129,53 @@ import cards3d from '@/components/Cards3dv.vue'
     width: 60%;
     text-align: center;
 }
+
+.comments {
+    width: 100%;
+    height: auto;
+    background: linear-gradient(135deg, #595cff 30%, #c6f8ff 50%);
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    justify-content: center;
+}
+
+.comments p {
+    font-size: 1.0rem;
+    color: #333;
+    margin-bottom: 10px;
+    margin-top: 5%;
+}
+
+
+.img-gif {
+  position: absolute;
+    top: 35px;
+    right: 78%;
+    width: 6%;
+    border-radius: 4%;
+    height: auto;
+}
+.img-gif2 {
+    position: absolute;
+    top: 35px;
+    left: 78%;
+    width: 6%;
+    border-radius: 4%;
+    height: auto;
+}
+
+input[type="text"] {
+    top: -20px;
+    width: 50%;
+    padding: 10px;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 1rem;
+    background-color: white;
+}
+
 
 </style>
